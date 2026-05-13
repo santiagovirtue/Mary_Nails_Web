7f4a524343eb46d76bedcb2641d8d7ab3926f914
Santiago Acevedo <sg5295506@gmail.com>
2026-05-13 23:54:29 +0000

    Agregar calificacion de servicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,21 +6,24 @@ import { Galeria } from './pages/galeria/galeria';
 import { Reservas } from './pages/reservas/reservas';
 import { Login } from './pages/login/login';
 
+import { MisCitas } from './cliente/mis-citas/mis-citas';
+import { CalificarServicio } from './client/calificar-servicio/calificar-servicio';
+
 import { LoginAdmin } from './admin/login-admin/login-admin';
 import { Dashboard } from './admin/dashboard/dashboard';
 import { Cronograma } from './admin/cronograma/cronograma';
 import { Disponibilidad } from './admin/disponibilidad/disponibilidad';
 
-import { MisCitas } from './cliente/mis-citas/mis-citas';
-
 export const routes: Routes = [
   { path: '', component: Home },
+
   { path: 'servicios', component: Servicios },
   { path: 'galeria', component: Galeria },
   { path: 'reservas', component: Reservas },
   { path: 'login', component: Login },
 
   { path: 'cliente/mis-citas', component: MisCitas },
+  { path: 'cliente/calificar-servicio', component: CalificarServicio },
 
   { path: 'admin/login', component: LoginAdmin },
   { path: 'admin/dashboard', component: Dashboard },
---
+++ src/app/client/calificar-servicio/calificar-servicio.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calificar-servicio',
+  imports: [],
+  templateUrl: './calificar-servicio.html',
+  styleUrl: './calificar-servicio.css',
+})
+export class CalificarServicio {}
